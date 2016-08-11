12c1918a317ba7b36270719a7d95155d5f94822b
Flemming Bregnvig <flemming@bregnvig.dk>
2016-08-11 12:57:01 +0200

    Fixed test bugs

--- examples/src/app/structural/unless.directive.spec.ts
+++ examples/src/app/structural/unless.directive.spec.ts
@@ -1,11 +1,2 @@
 /* tslint:disable:no-unused-variable */
 
-import { addProviders, async, inject } from '@angular/core/testing';
-import { Unless } from './unless.directive';
-
-describe('Directive: Unless', () => {
-  it('should create an instance', () => {
-    let directive = new Unless();
-    expect(directive).toBeTruthy();
-  });
-});
--- examples/src/app/structural/unless/unless.component.spec.ts
+++ examples/src/app/structural/unless/unless.component.spec.ts
@@ -1,13 +1,2 @@
 /* tslint:disable:no-unused-variable */
 
-import { By }           from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
-import { addProviders, async, inject } from '@angular/core/testing';
-import { UnlessComponent } from './unless.component';
-
-describe('Component: Unless', () => {
-  it('should create an instance', () => {
-    let component = new UnlessComponent();
-    expect(component).toBeTruthy();
-  });
-});
