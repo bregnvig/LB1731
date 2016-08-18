47717a97c88f24cf735533f759500ef2460f8eae
Flemming Bregnvig <flemming@bregnvig.dk>
2016-08-18 20:42:47 +0200

    Added leaflet to the solution

--- labs/sync-file.sh
+++ labs/sync-file.sh
@@ -5,6 +5,7 @@ cp ./03-create-first-component/$1 ./07-create-location-service/$1
 cp ./03-create-first-component/$1 ./08-add-some-pipes/$1
 cp ./03-create-first-component/$1 ./09-add-some-routes/$1
 
+cp ./03-create-first-component/$1 ../solutions/03-create-first-component/$1
 cp ./03-create-first-component/$1 ../solutions/04-create-footer-component/$1
 cp ./03-create-first-component/$1 ../solutions/05-create-playground-service/$1
 cp ./03-create-first-component/$1 ../solutions/06-add-ajax-to-playground-service/$1
