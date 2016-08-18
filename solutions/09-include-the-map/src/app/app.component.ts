c075914a0b766f038415867d0af27e043989d31d
Flemming Bregnvig <flemming@bregnvig.dk>
2016-08-18 21:57:55 +0200

    Added leaflet map to solution

--- solutions/09-include-the-map/src/app/app.component.ts
+++ solutions/09-include-the-map/src/app/app.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Observable } from 'rxjs/Observable';
+
 import { Playground, LocationService  } from './shared';
 
+import { Marker, Center } from './leaflet';
 
 @Component({
   moduleId: module.id,
@@ -13,6 +16,9 @@ export class AppComponent implements OnInit {
   title = 'app works!';
 
   public playground: Playground;
+  public markers: Observable<Marker>;
+  public center: Center = new Center(56.360029, 10.746635);
+
 
   constructor(private locationService: LocationService) {
   
@@ -20,8 +26,11 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     this.locationService.current.subscribe(location => {
+      this.center = new Center(location.lat, location.lng);
       console.log('Obtained location', location)
     });
+    this.markers = this.locationService.current.map(coordinate => new Marker('me', coordinate.lat, coordinate.lng, 'Her er jeg'));
+    
   }
 
   public playgroundSelected(playground: Playground): void {
