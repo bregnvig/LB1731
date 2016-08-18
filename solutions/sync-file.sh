46e041411d1897ad9d91b43f7f5834dbafa68173
Flemming Bregnvig <flemming@bregnvig.dk>
2016-08-18 21:20:51 +0200

    Renamed a lot

---
+++ solutions/sync-file.sh
@@ -0,0 +1,15 @@
+cp ./$2/$1 ./04-create-footer-component/$1
+cp ./$2/$1 ./05-create-playground-service/$1
+cp ./$2/$1 ./06-add-ajax-to-playground-service/$1
+cp ./$2/$1 ./07-create-location-service/$1
+cp ./$2/$1 ./08-add-some-pipes/$1
+cp ./$2/$1 ./09-add-some-routes/$1
+
+cp ./$2/$1 ../solutions/03-create-first-component/$1
+cp ./$2/$1 ../solutions/04-include-feature-module/$1
+cp ./$2/$1 ../solutions/04-create-footer-component/$1
+cp ./$2/$1 ../solutions/05-create-playground-service/$1
+cp ./$2/$1 ../solutions/06-add-ajax-to-playground-service/$1
+cp ./$2/$1 ../solutions/07-create-location-service/$1
+cp ./$2/$1 ../solutions/08-add-some-pipes/$1
+cp ./$2/$1 ../solutions/09-add-some-routes/$1
