fd03c7091ddedd3c3811418c0e166182c392cf9e
Flemming Bregnvig <flemming@bregnvig.dk>
2016-10-15 15:29:14 +0200

    Fixed solutions

--- solutions/sync-file.sh
+++ solutions/sync-file.sh
@@ -1,12 +1,12 @@
 # cp ./$2/$1 ./03-create-first-component/$1
-# cp ./$2/$1 ./04-include-feature-module/$1
-# cp ./$2/$1 ./04-include-feature-module/$1
-# cp ./$2/$1 ./05-create-footer-component/$1
-# cp ./$2/$1 ./06-create-playground-service/$1
-# cp ./$2/$1 ./07-add-ajax-to-playground-service/$1
-# cp ./$2/$1 ./08-create-location-service/$1
-# cp ./$2/$1 ./09-include-the-map/$1
-# cp ./$2/$1 ./10-add-some-pipes/$1
-# cp ./$2/$1 ./11-add-some-routes/$1
+cp ./$2/$1 ./04-include-feature-module/$1
+cp ./$2/$1 ./04-include-feature-module/$1
+cp ./$2/$1 ./05-create-footer-component/$1
+cp ./$2/$1 ./06-create-playground-service/$1
+cp ./$2/$1 ./07-add-ajax-to-playground-service/$1
+cp ./$2/$1 ./08-create-location-service/$1
+cp ./$2/$1 ./09-include-the-map/$1
+cp ./$2/$1 ./10-add-some-pipes/$1
+cp ./$2/$1 ./11-add-some-routes/$1
 cp ./$2/$1 ./12-add-filter-to-sidebar/$1
-# cp ./$2/$1 ./13-add-some-tests/$1
+cp ./$2/$1 ./13-add-some-tests/$1
