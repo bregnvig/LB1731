5b3af69c86c6b4d3e1c34b560f2d26cf69bb08c8
Flemming Bregnvig <flemming@bregnvig.dk>
2017-09-21 21:33:04 +0200

    Moved create playground service to presentation vs smart

--- solutions/06-create-playground-service/src/app/shared/playground.service.ts
+++ solutions/06-create-playground-service/src/app/shared/playground.service.ts
@@ -1,12 +1,10 @@
-import { Injectable } from '@angular/core';
-
-import { Playground } from './playground';
 import { MOCK_PLAYGROUNDS } from './mock-playgrounds';
+import { Playground } from './playground';
+import { Injectable } from '@angular/core';
 
 @Injectable()
 export class PlaygroundService {
-
-  public getPlaygrounds(): Playground[] {
-    return MOCK_PLAYGROUNDS;
-  }
+    public getPlaygrounds(): Playground[] {
+        return MOCK_PLAYGROUNDS;
+    }
 }
