5dbeb5aa2d100c4e4161193e0f15a4ca42446132
Flemming Bregnvig <flemming@bregnvig.dk>
2017-09-22 06:26:02 +0200

    Moved provide location service to presentation vs smart

--- solutions/08-create-location-service/src/app/app.module.ts
+++ solutions/08-create-location-service/src/app/app.module.ts
@@ -1,17 +1,31 @@
-import { NgModule }       from '@angular/core';
-import { BrowserModule  } from '@angular/platform-browser';
-import { AppComponent }   from './app.component';
-import { HttpModule } from '@angular/http';
+import { LocationService } from './shared/location.service';
+import { PlaygroundService } from './shared/playground.service';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
 
 import { LeafletModule } from './leaflet';
 import { SidebarComponent } from './sidebar';
 import { FooterComponent } from './footer';
-import { PlaygroundService, LocationService } from './shared';
+import { HttpModule } from '@angular/http';
 
 @NgModule({
-    declarations: [AppComponent, SidebarComponent, FooterComponent],
-    imports: [BrowserModule, LeafletModule, HttpModule],
-    providers: [PlaygroundService, LocationService],
+    declarations: [
+        AppComponent,
+        SidebarComponent,
+        FooterComponent
+    ],
+    imports: [
+        BrowserModule,
+        LeafletModule,
+        HttpModule
+    ],
+    providers: [
+        PlaygroundService,
+        LocationService,
+    ],
     bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {
+
+}
