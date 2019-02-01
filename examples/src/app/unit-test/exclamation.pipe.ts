33da78681321fbc227f7e26f9ff90a06eaef1207
Flemming Bregnvig <flemming@bregnvig.dk>
2019-02-01 08:43:45 +0100

    Better method signature

--- examples/src/app/unit-test/exclamation.pipe.ts
+++ examples/src/app/unit-test/exclamation.pipe.ts
@@ -5,7 +5,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class ExclamationPipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
+  transform(value: any): string {
     return (value || '') + '!';
   }
 
