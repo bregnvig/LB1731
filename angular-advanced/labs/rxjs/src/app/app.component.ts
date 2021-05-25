9e2065a2c591e71b6e89ef0ee07587833aebb99b
Flemming Bregnvig <flemming@bregnvig.dk>
2021-05-25 12:58:41 +0200

    Playground app works

--- angular-advanced/labs/rxjs/src/app/app.component.ts
+++ angular-advanced/labs/rxjs/src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { combineLatest, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Center, Marker } from './leaflet';
+import { Playground } from './model';
+import { LocationService, PlaygroundService } from './service';
 
 @Component({
   selector: 'loop-root',
@@ -6,5 +11,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'rxjs';
+
+  playgrounds: Playground[] = [];
+  playground: Playground | undefined;
+  center: Center = new Center(56.360029, 10.746635);
+  markers$: Observable<Marker> | undefined;
+
+  constructor(private service: PlaygroundService, private locationService: LocationService) {
+  }
+
+  ngOnInit() {
+    this.service.playgrounds$.subscribe(playgrounds => this.playgrounds = playgrounds);
+    this.locationService.location$.subscribe(location => {
+      this.center = new Center(location.lat, location.lng, 12);
+    });
+    this.markers$ = this.locationService.location$.pipe(
+      map(location => new Marker('me', location.lat, location.lng))
+    );
+
+    const getDistance = this.locationService.getDistance;
+    combineLatest([
+      this.service.playgrounds$,
+      this.locationService.location$
+    ]).pipe(
+      map(([playgrounds, location]) =>
+        playgrounds.sort((a: Playground, b: Playground) => getDistance(a.position, location) - getDistance(b.position, location))
+      )
+    );
+  }
+
+  playgroundSelected(playground: Playground): void {
+    this.playground = playground;
+    console.log('Playground selected', playground);
+  }
 }
--- angular-advanced/labs/rxjs/src/app/app.module.ts
+++ angular-advanced/labs/rxjs/src/app/app.module.ts
@@ -1,16 +1,32 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { LeafletModule } from './leaflet';
+import { DefaultDescriptionPipe, DistancePipe, HumanizeDistancePipe } from './pipe';
+import { SidebarComponent } from './sidebar/sidebar.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    FooterComponent,
+    DistancePipe,
+    HumanizeDistancePipe,
+    DefaultDescriptionPipe,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    NgbModule,
+    LeafletModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
