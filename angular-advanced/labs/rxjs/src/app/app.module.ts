5796721aefebc10e834271bc80c7a1bce22cbd08
Flemming Bregnvig <flemming@bregnvig.dk>
2021-06-10 18:06:19 +0200

    Add labs

---
+++ angular-advanced/labs/rxjs/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { LeafletModule } from './leaflet';
+import { DefaultDescriptionPipe, DistancePipe, HumanizeDistancePipe } from './pipe';
+import { SidebarComponent } from './sidebar/sidebar.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SidebarComponent,
+    FooterComponent,
+    DistancePipe,
+    HumanizeDistancePipe,
+    DefaultDescriptionPipe,
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    NgbModule,
+    LeafletModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
