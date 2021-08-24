5d58fe72e585f2efe34d203a2ef6befa6c30cf3f
Flemming Bregnvig <flemming@bregnvig.dk>
2021-08-24 20:45:05 +0200

    Remove protractor

---
+++ solutions/delete-file.sh
@@ -0,0 +1,11 @@
+#cp ./$2/$1 ./03-create-first-component/$1
+rm ./04-include-feature-module/$1
+rm ./05-create-footer-component/$1
+rm ./06-create-playground-service/$1
+rm ./07-add-ajax-to-playground-service/$1
+rm ./08-create-location-service/$1
+rm ./09-include-the-map/$1
+rm ./10-add-some-pipes/$1
+rm ./11-add-some-routes/$1
+rm ./12-add-filter-to-sidebar/$1
+# cp ./$2/$1 ./13-add-some-tests/$1
