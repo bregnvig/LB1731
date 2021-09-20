778156e7830371f506cf6d90d8c1e63a093959e9
Johannes <johannes@nestech.dk>
2021-09-20 22:40:39 +0200

    Add unit test solution

---
+++ angular-advanced/solutions/unit-test/src/app/model/playground.ts
@@ -0,0 +1,9 @@
+import { Coordinate } from './coordinate';
+
+export interface Playground {
+  readonly id: string;
+  name: string;
+  addressDescription?: string;
+  description?: string;
+  position: Coordinate;
+}
