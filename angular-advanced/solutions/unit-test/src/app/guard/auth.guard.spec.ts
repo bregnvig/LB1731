72db4fc847290314a83141f45aa8527c68cf6d3b
Johannes Normann Jensen <johannes@nestech.dk>
2021-09-26 19:27:43 +0200

    xdescribe

--- angular-advanced/solutions/unit-test/src/app/guard/auth.guard.spec.ts
+++ angular-advanced/solutions/unit-test/src/app/guard/auth.guard.spec.ts
@@ -1,8 +1,8 @@
 import { TestBed } from '@angular/core/testing';
-
 import { AuthGuard } from './auth.guard';
 
-describe('AuthGuard', () => {
+
+xdescribe('AuthGuard', () => {
   let guard: AuthGuard;
 
   beforeEach(() => {
