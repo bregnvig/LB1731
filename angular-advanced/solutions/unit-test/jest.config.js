49943ad6ec3654dee4ee4db0a9de056d4e0e3b69
Johannes Normann Jensen <johannes@nestech.dk>
2021-09-26 15:29:56 +0200

    Add ts-jest to globals

--- angular-advanced/solutions/unit-test/jest.config.js
+++ angular-advanced/solutions/unit-test/jest.config.js
@@ -2,6 +2,11 @@ module.exports = {
   moduleNameMapper: {
     '@core/(.*)': '<rootDir>/src/app/core/$1',
   },
+  globals: {
+    'ts-jest': {
+      isolatedModules: true,
+    },
+  },
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
 };
