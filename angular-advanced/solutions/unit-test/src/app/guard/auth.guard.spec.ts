821631c7fe5f99bc0f042a6cc8302e6cb66e3c74
Johannes Normann Jensen <johannes@nestech.dk>
2021-09-26 22:59:08 +0200

    Add Auth guard spec

--- angular-advanced/solutions/unit-test/src/app/guard/auth.guard.spec.ts
+++ angular-advanced/solutions/unit-test/src/app/guard/auth.guard.spec.ts
@@ -1,16 +1,33 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AuthService } from '../service/auth.service';
 import { AuthGuard } from './auth.guard';
 
 
-xdescribe('AuthGuard', () => {
-  let guard: AuthGuard;
+describe('AuthGuard', () => {
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    guard = TestBed.inject(AuthGuard);
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+        HttpClientTestingModule,
+      ]
+    });
   });
 
-  it('should be created', () => {
-    expect(guard).toBeTruthy();
+  it('should return true when AuthService isLoggedIn is true', () => {
+    TestBed.overrideProvider(AuthService, { useValue: { isLoggedIn: true } }); // <= Stub
+    const guard = TestBed.inject(AuthGuard);
+    expect(guard.canActivate({} as any, {} as any)).toEqual(true);
   });
+
+  it('should return true when AuthService isLoggedIn is true', () => {
+    TestBed.overrideProvider(AuthService, { useValue: { isLoggedIn: false } }); // <= Stub
+    const guard = TestBed.inject(AuthGuard);
+    const router = TestBed.inject(Router);
+    expect(guard.canActivate({} as any, {} as any)).toEqual(router.createUrlTree(['/login']));
+  });
+
 });
