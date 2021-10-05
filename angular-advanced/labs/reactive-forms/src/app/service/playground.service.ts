ca16d12e84143e6180f0c3b4a204da89714fac71
Flemming Bregnvig <flemming@bregnvig.dk>
2021-10-05 20:20:36 +0200

    Add phony update

--- angular-advanced/labs/reactive-forms/src/app/service/playground.service.ts
+++ angular-advanced/labs/reactive-forms/src/app/service/playground.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, combineLatest, Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
 import { Playground } from '../model';
 
 @Injectable({
@@ -10,7 +11,20 @@ export class PlaygroundService {
 
   playgrounds$: Observable<Playground[]>;
 
+  private altered$ = new BehaviorSubject<Playground[]>([]);
+
   constructor(http: HttpClient) {
-    this.playgrounds$ = http.get<Playground[]>('assets/copenhagen.json');
+
+    this.playgrounds$ = combineLatest([
+      http.get<Playground[]>('assets/copenhagen.json'),
+      this.altered$,
+    ]).pipe(
+      map(([playgrounds, altered]) => playgrounds.map(p => altered.find(({ id }) => id === p.id) ?? p)),
+      shareReplay(1),
+    );
+  }
+
+  update(playground: Playground) {
+    this.altered$.next([...this.altered$.value.filter(p => p.id !== playground.id), playground]);
   }
 }
