72896ba995d93d32ef9e000ffa2238aa6e000181
Johannes Normann Jensen <johannes@nestech.dk>
2021-11-06 20:29:48 +0100

    Add solutions exercise-2

---
+++ angular-advanced/solutions/routing/exercise-2/src/app/sidebar/sidebar.component.ts
@@ -0,0 +1,22 @@
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Coordinate, Playground } from '../model';
+import { LocationService } from '../service';
+
+@Component({
+  selector: 'loop-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SidebarComponent {
+
+  @Input() playgrounds: Playground[] | null | undefined = [];
+  @Input() selectedPlayground: Playground | null | undefined = null;
+  @Output() edit = new EventEmitter<Playground>();
+
+  location$: Observable<Coordinate> = this.locationService.location$;
+
+  constructor(private locationService: LocationService) { }
+
+}
