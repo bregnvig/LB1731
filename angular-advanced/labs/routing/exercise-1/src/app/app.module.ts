1613bed38d465d109379bf9cabe55c2a9bb48d34
Flemming Bregnvig <flemming@bregnvig.dk>
2021-11-06 10:30:04 +0100

    Remove routijng module

--- angular-advanced/labs/routing/exercise-1/src/app/app.module.ts
+++ angular-advanced/labs/routing/exercise-1/src/app/app.module.ts
@@ -8,14 +8,13 @@ import { far } from '@fortawesome/free-regular-svg-icons';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { DynamicIoModule } from 'ng-dynamic-component';
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EditPlaygroundControlComponent } from './edit-playground-control/edit-playground-control.component';
 import { EditPlaygroundModalComponent } from './edit-playground-modal/edit-playground-modal.component';
 import { FooterComponent } from './footer/footer.component';
 import { LeafletModule } from './leaflet';
 import { DefaultDescriptionPipe, DistancePipe, HumanizeDistancePipe } from './pipe';
 import { SidebarComponent } from './sidebar/sidebar.component';
-import { EditPlaygroundControlComponent } from './edit-playground-control/edit-playground-control.component';
 
 library.add(fas, far);
 
@@ -34,7 +33,6 @@ library.add(fas, far);
     BrowserModule,
     HttpClientModule,
     ReactiveFormsModule,
-    AppRoutingModule,
     NgbModule,
     LeafletModule,
     FontAwesomeModule,
