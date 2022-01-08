7efed8e13a5896973462eaf72f086b834fb27280
Johannes Normann Jensen <johannes@nestech.dk>
2022-01-08 14:53:32 +0100

    Add p&d lab 3

---
+++ angular-advanced/labs/pipes-and-directives/exercise-3/src/app/login/login.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthService } from '../service/auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+
+  fg = this.fb.group({
+    email: [sessionStorage.getItem('email'), [Validators.required, Validators.email]],
+    password: ['somestring', Validators.required],
+  });
+
+  constructor(
+    private route: ActivatedRoute,
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router,
+  ) { }
+
+  login(): void {
+    const { email, password } = this.fg.value;
+    if (this.fg.valid) {
+      this.authService.login(email, password).subscribe(value => {
+        if (value) {
+          this.router.navigate(['/']);
+        }
+      });
+    }
+  }
+
+}
