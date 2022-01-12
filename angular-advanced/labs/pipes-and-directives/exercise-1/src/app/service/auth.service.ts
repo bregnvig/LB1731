42f7ae4fad5db82090c6208aa48d63af7ee464bc
Flemming Bregnvig <flemming@bregnvig.dk>
2022-01-12 21:06:13 +0100

    Use new version

--- angular-advanced/labs/pipes-and-directives/exercise-1/src/app/service/auth.service.ts
+++ angular-advanced/labs/pipes-and-directives/exercise-1/src/app/service/auth.service.ts
@@ -1,11 +1,19 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+export type Role = 'anonymous' | 'admin';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  readonly role$ = new BehaviorSubject<'anonymous' | 'admin'>('anonymous');
+  readonly role$ = new BehaviorSubject<Role>('anonymous');
 
+  isInRole$(role: Role): Observable<boolean> {
+    return this.role$.pipe(
+      map(currentRole => role === 'anonymous' || currentRole === 'admin')
+    );
+  }
 }
