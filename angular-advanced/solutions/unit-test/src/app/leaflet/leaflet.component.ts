da56620e5790e777720505ddab98d008b6a008d7
Johannes Normann Jensen <johannes@nestech.dk>
2022-03-21 21:24:21 +0100

    Change tileLayer

--- angular-advanced/solutions/unit-test/src/app/leaflet/leaflet.component.ts
+++ angular-advanced/solutions/unit-test/src/app/leaflet/leaflet.component.ts
@@ -29,7 +29,7 @@ export class LeafletComponent implements AfterViewInit, OnDestroy {
 
   constructor() {
     this.baseMaps = {
-      OpenStreetMap: tileLayer('///{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      OpenStreetMap: tileLayer('https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
       })
     };
