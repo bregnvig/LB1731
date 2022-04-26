22183eb42bc4d205365b89fc7da509543148c25b
Flemming Bregnvig <flemming@bregnvig.dk>
2022-04-26 12:16:32 +0200

    Minor fixes

--- angular-advanced/demo/src/polyfills.ts
+++ angular-advanced/demo/src/polyfills.ts
@@ -45,8 +45,8 @@
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
+import '@angular/localize/init';
 import 'zone.js'; // Included with Angular CLI.
-
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
