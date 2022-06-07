e89434d50a18782c139d60c420b1491c37303f8d
Flemming Bregnvig <flemming@bregnvig.dk>
2022-06-07 08:22:03 +0200

    Quick paclaging of foundation

---
+++ zipit.sh
@@ -0,0 +1,4 @@
+
+zip -r solutions solutions -x "**/node_modules/*" -x "**/.angular/*" -x "**/di/*" -x ".git" -x "**/*.sh"
+zip -r examples examples -x "**/node_modules/*" -x "**/.angular/*" -x "**/di/*" -x ".git" -x "**/*.sh"
+zip -r labs labs -x "**/node_modules/*" -x "**/.angular/*" -x "**/di/*" -x ".git" -x "**/*.sh"
