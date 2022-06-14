b6a6e24c7dfa4ea385c0be1a8241363508c41675
Flemming Bregnvig <flemming@bregnvig.dk>
2022-06-14 08:12:00 +0200

    Rename

--- examples/src/app/form/person-form-7/person-form-7.component.ts
+++ examples/src/app/form/person-form-7/person-form-7.component.ts
@@ -1,7 +1,7 @@
-import { Person } from './../person';
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Person } from './../person';
 
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-person-form-7',
@@ -10,27 +10,28 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 })
 export class PersonForm7Component implements OnInit {
 
-   colors = ["Red", "Green", "Blue"];
-   model = new Person('Flemming', 'Bregnvig', "Blue", 182);
+  colors = ["Red", "Green", "Blue"];
+  model = new Person('Flemming', 'Bregnvig', "Blue", 182);
 
-   myForm: FormGroup;
+  fg: FormGroup;
 
   constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
-    this.myForm = this.formBuilder.group({
+    const nameValidator = (control: AbstractControl) => control.value === 'Flemming' ? null : { nameIsWrong: true };
+    this.fg = this.formBuilder.group({
       name: this.formBuilder.group({
-        firstName: [this.model.firstName, Validators.required],
+        firstName: [this.model.firstName, [Validators.required, nameValidator]],
         lastName: [this.model.lastName, Validators.required]
       }),
       favoriteColor: [],
       height: [undefined, [Validators.min(100), Validators.max(200)]]
-    })
-    
+    });
+
   }
 
-   onSubmit() {
-    console.log('Value', this.myForm.value);
+  onSubmit() {
+    console.log('Value', { ...this.model, ...this.fg.value });
 
   }
 
