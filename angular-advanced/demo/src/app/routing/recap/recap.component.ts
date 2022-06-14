49f3d0f5991f9be41cd560722d12076396548c8a
Flemming Bregnvig <flemming@bregnvig.dk>
2022-06-14 12:16:06 +0200

    Mor eoptions

--- angular-advanced/demo/src/app/routing/recap/recap.component.ts
+++ angular-advanced/demo/src/app/routing/recap/recap.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -17,12 +17,21 @@ export class RecapComponent extends AbstractSubscribeUnsubscribeDirective implem
   private row: number | undefined;
 
   transform$!: Observable<string>;
+  links!: any[];
 
   constructor(private router: Router, public route: ActivatedRoute) {
     super();
   }
 
   ngOnInit(): void {
+
+    this.links = [
+      { label: 'Show all', url: [{}] },
+      { label: 'show 1', url: [{ row: 1 }] },
+      { label: 'show 2', url: [{ row: 2 }] },
+      { label: 'show 3', url: [{ row: 3 }] },
+    ];
+
     this.transform$ = this.route.params.pipe(
       map<Params, Transform>(params => params['transform']),
       map(transform => `text-${transform}`)
