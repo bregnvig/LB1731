09fa262c786f614e3e9cc260bfc3e265c59b4858
Johannes Normann Jensen <johannesnormannjensen@gmail.com>
2022-11-17 08:51:12 +0100

    fix: stop excluding di

--- angular-advanced/zipit.sh
+++ angular-advanced/zipit.sh
@@ -1,4 +1,4 @@
 
-zip -r solutions solutions -x "**/node_modules/*" -x "**/.angular/*" -x "**/di/*"
-zip -r demo demo -x "**/node_modules/*" -x "**/.angular/*" -x "**/di/*"
-zip -r labs labs -x "**/node_modules/*" -x "**/.angular/*" -x "**/di/*"
+zip -r solutions solutions -x "**/node_modules/*" -x "**/.angular/*"
+zip -r demo demo -x "**/node_modules/*" -x "**/.angular/*"
+zip -r labs labs -x "**/node_modules/*" -x "**/.angular/*"
