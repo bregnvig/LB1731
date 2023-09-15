c39d0fd0d7fb91b535a99ef9feed2cd8fe7d7887
Flemming Bregnvig <flemming@bregnvig.dk>
2023-09-15 06:57:49 +0200

    Feature module removed

--- labs/04-create-footer-component/src/app/app.module.ts
+++ labs/04-create-footer-component/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LeafletModule } from './leaflet';
 import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
@@ -16,7 +15,6 @@ import { SidebarComponent } from './sidebar/sidebar.component';
     BrowserModule,
     AppRoutingModule,
     NgbModule,
-    LeafletModule
   ],
   providers: [],
   bootstrap: [AppComponent]
