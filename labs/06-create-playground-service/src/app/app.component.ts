ef023a1ace9904492c407aee6a628a4e8326d173
Flemming Bregnvig <flemming@bregnvig.dk>
2023-09-15 07:04:17 +0200

    Switch exercises

--- labs/06-create-playground-service/src/app/app.component.ts
+++ labs/06-create-playground-service/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Center } from './leaflet';
 import { MOCK_PLAYGROUNDS } from './shared/mock-playgrounds';
 import { Playground } from './shared/playground';
 
@@ -11,6 +12,10 @@ export class AppComponent {
 
   appPlaygrounds: Playground[] = MOCK_PLAYGROUNDS;
   playground?: Playground;
+  center: Center = {
+    lat: 56.360029,
+    lng: 10.746635
+  };
 
   playgroundSelected(playground: Playground) {
     console.log(playground);
