186cd0b75c34727dcd549c9443856f91fbfc3c69
Flemming Bregnvig <flemming@bregnvig.dk>
2023-09-15 06:46:11 +0200

    Align with new exercises

--- solutions/06-create-playground-service/src/app/app.component.ts
+++ solutions/06-create-playground-service/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Center } from './leaflet';
+import { MOCK_PLAYGROUNDS } from './shared/mock-playgrounds';
 import { Playground } from './shared/playground';
 import { PlaygroundService } from './shared/playground.service';
 
@@ -11,10 +13,15 @@ export class AppComponent {
 
   appPlaygrounds: Playground[];
   playground?: Playground;
+  center: Center = {
+    lat: 56.360029,
+    lng: 10.746635
+  };
 
   constructor(service: PlaygroundService) {
     this.appPlaygrounds = service.getPlaygrounds();
   }
+
 
   playgroundSelected(playground: Playground) {
     console.log(playground);
