3f778b6e361c44fb102d82fdcbf57f2d2c97e2f4
Flemming Bregnvig <flemming@bregnvig.dk>
2023-10-03 09:28:10 +0200

    Switch functional guard

--- angular-advanced/solutions/routing/exercise-1/src/app/utils/rxjs-utils.ts
+++ angular-advanced/solutions/routing/exercise-1/src/app/utils/rxjs-utils.ts
@@ -3,6 +3,6 @@ import { filter, shareReplay } from "rxjs/operators";
 
 export const withLength = <T>() => pipe(filter<T[]>(array => array?.length > 0));
 export const shareLatest = <T>() => pipe(shareReplay<T>({ bufferSize: 1, refCount: true }));
-export const truthy = <T>() => pipe(filter((a: T) => !!a));
-// export const truthy = <T>() => pipe(filter(x => !!x) as OperatorFunction<T | null | undefined | '' | 0, T>);
+// export const truthy = <T>() => pipe(filter((a: T) => !!a));
+export const truthy = <T>() => pipe(filter(x => !!x) as OperatorFunction<T | null | undefined | '' | 0, T>);
 
--- angular-advanced/solutions/routing/exercise-3/src/app/app-routing.module.ts
+++ angular-advanced/solutions/routing/exercise-3/src/app/app-routing.module.ts
@@ -1,13 +1,24 @@
-import { RouterModule, Routes } from '@angular/router';
-import { AuthGuard } from './guard';
+import { inject } from '@angular/core';
+import { ActivatedRouteSnapshot, Router, RouterModule, RouterStateSnapshot, Routes } from '@angular/router';
+import { map } from 'rxjs';
 import { LoginComponent } from './login/login.component';
 import { MapComponent } from './map/map.component';
+import { AuthService } from './service';
+
+
+const authGuard = (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
+  const router = inject(Router);
+  return inject(AuthService).isLoggedIn$.pipe(
+    map(isLoggedIn => isLoggedIn || router.createUrlTree(['/login', { returnUrl: state.url }])),
+  );
+};
+
 
 const routes: Routes = [
   {
     path: '',
     component: MapComponent,
-    canActivate: [AuthGuard],
+    canActivate: [authGuard],
   },
   {
     path: 'login',
@@ -16,7 +27,7 @@ const routes: Routes = [
   {
     path: ':id',
     component: MapComponent,
-    canActivate: [AuthGuard],
+    canActivate: [authGuard],
   },
 ];
 
