deccf4161e09806349662416aa660c0d7589935a
Flemming Bregnvig <flemming@bregnvig.dk>
2023-10-03 08:16:09 +0200

    v16

--- angular-advanced/solutions/routing/exercise-2/src/app/guard/auth.guard.ts
+++ angular-advanced/solutions/routing/exercise-2/src/app/guard/auth.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { AuthService } from '../service/auth.service';
@@ -7,7 +7,7 @@ import { AuthService } from '../service/auth.service';
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard  {
 
   constructor(private authService: AuthService, private router: Router) { }
 
