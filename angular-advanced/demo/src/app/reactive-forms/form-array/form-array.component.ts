52cf539ef4210d9addfabe091b761c44724de36f
Flemming Bregnvig <flemming@bregnvig.dk>
2023-11-05 11:34:38 +0100

    Add log to form array

--- angular-advanced/demo/src/app/reactive-forms/form-array/form-array.component.ts
+++ angular-advanced/demo/src/app/reactive-forms/form-array/form-array.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { debounceTime } from 'rxjs';
 import { AbstractSubscribeUnsubscribeDirective } from 'src/app/rxjs/rxjs-utils';
 
 @Component({
@@ -19,12 +20,17 @@ export class FormArrayComponent extends AbstractSubscribeUnsubscribeDirective im
 
   ngOnInit(): void {
     this.fg.controls.emailAddresses.valueChanges.pipe(
+      debounceTime(400),
       this.takeUntilDestroyed()
     ).subscribe(_ => console.log(_));
   }
 
   addEmail(value: string) {
     this.fg.controls.emailAddresses.push(this.fb.control(value, Validators.email));
+  }
+
+  save() {
+    console.log(this.fg.value);
   }
 
 }
