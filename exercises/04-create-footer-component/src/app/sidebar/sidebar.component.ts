959563a3972dcbfd0979a07cd5c5dd58b243313d
Flemming Bregnvig <flemming@bregnvig.dk>
2024-02-25 11:49:47 +0100

    Remove ngFor

--- exercises/04-create-footer-component/src/app/sidebar/sidebar.component.ts
+++ exercises/04-create-footer-component/src/app/sidebar/sidebar.component.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
 import { MOCK_PLAYGROUNDS } from '../shared/mock-playgrounds';
 import { Playground } from '../shared/playground';
-import { NgFor } from '@angular/common';
 
 @Component({
-    selector: 'app-sidebar',
-    templateUrl: './sidebar.component.html',
-    styleUrls: ['./sidebar.component.scss'],
-    standalone: true,
-    imports: [NgFor]
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.scss'],
+  standalone: true,
 })
 export class SidebarComponent {
   playgrounds: Playground[] = MOCK_PLAYGROUNDS;
