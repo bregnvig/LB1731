31ffa5a36cd8f40a440302d1ccd757ae42e8ca53
Flemming Bregnvig <flemming@bregnvig.dk>
2024-02-25 11:04:29 +0100

    Move solutions to v17

--- solutions/sync-file.sh
+++ solutions/sync-file.sh
@@ -1,5 +1,5 @@
 #cp ./$2/$1 ./03-create-first-component/$1
-cp ./$2/$1 ../labs/03-create-first-component/$1
+cp ./$2/$1 ../exercises/03-create-first-component/$1
 cp ./$2/$1 ./04-create-footer-component/$1
 cp ./$2/$1 ./05-include-feature-module/$1
 cp ./$2/$1 ./06-create-playground-service/$1
