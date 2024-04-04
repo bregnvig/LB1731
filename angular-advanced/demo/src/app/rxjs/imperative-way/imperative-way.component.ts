19046ae04b637b5031e3e842715d4a36c2911296
Flemming Bregnvig <flemming@bregnvig.dk>
2024-04-04 08:23:38 +0200

    Minor v17 stuff

--- angular-advanced/demo/src/app/rxjs/imperative-way/imperative-way.component.ts
+++ angular-advanced/demo/src/app/rxjs/imperative-way/imperative-way.component.ts
@@ -11,7 +11,6 @@ export class ImperativeWayComponent implements OnInit, OnDestroy {
 
   playgrounds: Playground[] = [];
   term: string = '';
-  trackById = (i: number, playground: Playground) => playground.id;
 
   private fetchedPlaygrounds: Playground[] = this.playgrounds;
   private location: Coordinate | undefined;
@@ -43,7 +42,7 @@ export class ImperativeWayComponent implements OnInit, OnDestroy {
     console.log('Filtering by', term);
     this.playgrounds = this.fetchedPlaygrounds.filter(p => p.name.toLocaleLowerCase().includes(term));
     // Lets say I forgot to call this here
-    // this.sortPlaygrounds();
+    this.sortPlaygrounds();
   }
 
   getDistance(playground: Playground): string | undefined {
