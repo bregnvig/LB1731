a7255912250aba15ada0539a114a9cff2efba924
Flemming Bregnvig <flemming@bregnvig.dk>
2024-04-18 09:05:45 +0200

    Guards

--- angular-advanced/labs/routing/exercise-3/src/app/app.routing.ts
+++ angular-advanced/labs/routing/exercise-3/src/app/app.routing.ts
@@ -1,13 +1,32 @@
-import { Routes } from '@angular/router';
+import { inject } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivateFn, Router, Routes } from '@angular/router';
+import { first, map } from 'rxjs';
+import { LoginComponent } from './login/login.component';
 import { MapComponent } from './map/map.component';
+import { AuthService } from './service';
+
+const authGuard: CanActivateFn = (route: ActivatedRouteSnapshot) => {
+  const service = inject(AuthService);
+  const router = inject(Router);
+  return service.isLoggedIn$.pipe(
+    first(),
+    map(isLoggedIn => isLoggedIn || router.createUrlTree(['/login', { returnUrl: route.url }])),
+  );
+};
 
 export const AppRoutes: Routes = [
   {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
     path: '',
+    canActivate: [authGuard],
     component: MapComponent,
   },
   {
     path: ':id',
+    canActivate: [authGuard],
     component: MapComponent,
   },
 ];
