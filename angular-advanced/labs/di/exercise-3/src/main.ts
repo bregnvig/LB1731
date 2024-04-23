06dfa4cf97bf25b2a5f827d09cf1638ac0749302
Flemming Bregnvig <flemming@bregnvig.dk>
2024-04-23 07:46:37 +0200

    Copy solutions to labs

--- angular-advanced/labs/di/exercise-3/src/main.ts
+++ angular-advanced/labs/di/exercise-3/src/main.ts
@@ -1,12 +1,26 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
 
-import { AppModule } from './app/app.module';
+
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AppComponent } from './app/app.component';
+import { PLAYGROUND_URL } from './app/service';
 import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(BrowserModule, ReactiveFormsModule, NgbModule),
+    {
+      provide: PLAYGROUND_URL,
+      useValue: environment.playgroundsURL,
+    },
+    provideHttpClient(withInterceptorsFromDi())
+  ]
+})
   .catch(err => console.error(err));
