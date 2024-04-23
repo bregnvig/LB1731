05480c960f60af7d3c4ec41de0c76bea2e4e7e95
Flemming Bregnvig <flemming@bregnvig.dk>
2024-04-23 07:15:21 +0200

    Standalone & control flow

--- angular-advanced/solutions/di/exercise-3/src/main.ts
+++ angular-advanced/solutions/di/exercise-3/src/main.ts
@@ -1,36 +1,33 @@
-import { enableProdMode, APP_INITIALIZER, importProvidersFrom } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { APP_INITIALIZER, enableProdMode, importProvidersFrom } from '@angular/core';
 
 
-import { environment } from './environments/environment';
-import { AppComponent } from './app/app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { AppRoutingModule } from './app/app-routing.module';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
-import { withInterceptorsFromDi, provideHttpClient } from '@angular/common/http';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
-import { timer, switchMap } from 'rxjs';
-import { environment as environment_1 } from 'src/environments/environment';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { switchMap, timer } from 'rxjs';
+import { AppComponent } from './app/app.component';
 import { PLAYGROUND_URL, PlaygroundService } from './app/service';
+import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
 }
 
 bootstrapApplication(AppComponent, {
-    providers: [
-        importProvidersFrom(BrowserModule, ReactiveFormsModule, AppRoutingModule, NgbModule),
-        {
-            provide: PLAYGROUND_URL,
-            useValue: environment.playgroundsURL,
-        },
-        {
-            provide: APP_INITIALIZER,
-            useFactory: (service: PlaygroundService) => () => timer(2000).pipe(switchMap(() => service.playgrounds$)),
-            deps: [PlaygroundService],
-            multi: true,
-        },
-        provideHttpClient(withInterceptorsFromDi())
-    ]
+  providers: [
+    importProvidersFrom(BrowserModule, ReactiveFormsModule, NgbModule),
+    {
+      provide: PLAYGROUND_URL,
+      useValue: environment.playgroundsURL,
+    },
+    {
+      provide: APP_INITIALIZER,
+      useFactory: (service: PlaygroundService) => () => timer(2000).pipe(switchMap(() => service.playgrounds$)),
+      deps: [PlaygroundService],
+      multi: true,
+    },
+    provideHttpClient(withInterceptorsFromDi())
+  ]
 })
   .catch(err => console.error(err));
