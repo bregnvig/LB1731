23a63e04df2ed2ee36bcb6aeda953a0db852b391
Flemming Bregnvig <flemming@nestech.dk>
2024-05-14 09:58:04 +0200

    Fix zipping

--- angular-advanced/zipit.sh
+++ angular-advanced/zipit.sh
@@ -1,4 +1,3 @@
-
 zip -r solutions solutions -x "**/node_modules/*" -x "**/dist/*" -x "**/.angular/*" -x "**/*.sh"
 zip -r demo demo -x "**/node_modules/*" -x "**/dist/*" -x "**/.angular/*" -x "**/*.sh"
 zip -r labs labs -x "**/node_modules/*" -x "**/dist/*" -x "**/.angular/*" -x "**/*.sh"
