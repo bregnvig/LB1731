583b59ea317cb7cd8f45e0c07aa31c3a00c85ecb
Flemming Bregnvig <flemming@bregnvig.dk>
2024-05-22 19:53:15 +0200

    Clean up

--- angular-advanced/solutions/reactive-forms/exercise-3/src/main.ts
+++ angular-advanced/solutions/reactive-forms/exercise-3/src/main.ts
@@ -1,25 +1,34 @@
-import { enableProdMode, importProvidersFrom } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { APP_INITIALIZER, enableProdMode, importProvidersFrom } from '@angular/core';
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
-import { AppComponent } from './app/app.component';
-import { DynamicIoModule } from 'ng-dynamic-component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { AppRoutingModule } from './app/app-routing.module';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
-import { withInterceptorsFromDi, provideHttpClient } from '@angular/common/http';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { DynamicIoModule } from 'ng-dynamic-component';
+import { AppComponent } from './app/app.component';
+import { environment } from './environments/environment';
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+
+library.add(fas, far);
 
 if (environment.production) {
   enableProdMode();
 }
 
 bootstrapApplication(AppComponent, {
-    providers: [
-        importProvidersFrom(BrowserModule, ReactiveFormsModule, AppRoutingModule, NgbModule, FontAwesomeModule, DynamicIoModule),
-        provideHttpClient(withInterceptorsFromDi())
-    ]
-})
-  .catch(err => console.error(err));
+  providers: [
+    importProvidersFrom(BrowserModule, ReactiveFormsModule, NgbModule, FontAwesomeModule, DynamicIoModule),
+    provideHttpClient(withInterceptorsFromDi()),
+    {
+      provide: APP_INITIALIZER,
+      multi: true,
+      useFactory: (library: FaIconLibrary) => () => library.addIconPacks(fas),
+      deps: [FaIconLibrary]
+    },
+
+  ]
+}).catch(err => console.error(err));
