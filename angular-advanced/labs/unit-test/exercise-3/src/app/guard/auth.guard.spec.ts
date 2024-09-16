a50637dc5d52a3af9c16d6b1dca1bbe89623d587
Johannes Normann Jensen <johannes@normannjensen.dk>
2024-09-17 00:10:17 +0200

    skip guard tests

--- angular-advanced/labs/unit-test/exercise-3/src/app/guard/auth.guard.spec.ts
+++ angular-advanced/labs/unit-test/exercise-3/src/app/guard/auth.guard.spec.ts
@@ -3,8 +3,12 @@ import { ActivatedRouteSnapshot, Router, RouterStateSnapshot } from '@angular/ro
 import { AuthService } from '../service/auth.service';
 import { authGuard } from './auth.guard';
 
+/**
+ * Test suite below is skipped out because of missing AuthService to begin with. 
+ * When AuthService is created and isLoggedIn is implemented feel free "unskip" the suite and try the tests out
+ */
 
-describe('AuthGuard', () => {
+describe.skip('AuthGuard', () => {
 
   it('should return true when AuthService isLoggedIn is true', () => {
     // Arrange
