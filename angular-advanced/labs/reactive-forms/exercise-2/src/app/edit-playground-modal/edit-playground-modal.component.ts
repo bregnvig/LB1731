5d16ad54a6e5e54b24a0cc9e960aecc8d6e771e1
Flemming Bregnvig <flemming@bregnvig.dk>
2024-10-22 09:19:23 +0200

    Mlve v18 to labs

--- angular-advanced/labs/reactive-forms/exercise-2/src/app/edit-playground-modal/edit-playground-modal.component.ts
+++ angular-advanced/labs/reactive-forms/exercise-2/src/app/edit-playground-modal/edit-playground-modal.component.ts
@@ -4,11 +4,11 @@ import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { Playground } from '../model';
 
 @Component({
-    selector: 'loop-edit-playground-modal',
-    templateUrl: './edit-playground-modal.component.html',
-    styleUrls: ['./edit-playground-modal.component.scss'],
-    standalone: true,
-    imports: [ReactiveFormsModule]
+  selector: 'loop-edit-playground-modal',
+  templateUrl: './edit-playground-modal.component.html',
+  styleUrls: ['./edit-playground-modal.component.scss'],
+  standalone: true,
+  imports: [ReactiveFormsModule]
 })
 export class EditPlaygroundModalComponent implements OnInit {
 
@@ -24,12 +24,14 @@ export class EditPlaygroundModalComponent implements OnInit {
     addressDescription: this.fb.control(''),
   });
 
-  playground!: Playground;
+  playground?: Playground;
 
-  constructor(private fb: FormBuilder, public modal: NgbActiveModal) { }
+  constructor(private fb: FormBuilder, public modal: NgbActiveModal) {
+  }
 
   ngOnInit(): void {
-    this.fg.reset(this.playground || {});
+
+    this.fg.reset(this.playground ?? {});
   }
 
   initialize(playground: Playground) {
