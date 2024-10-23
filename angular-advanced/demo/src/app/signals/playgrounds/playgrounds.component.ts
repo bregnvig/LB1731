0b65f2233c40c161d9d55e7c8cd2919545443fbb
Johannes Normann Jensen <johannes@normannjensen.dk>
2024-10-24 00:28:44 +0200

    rm card

--- angular-advanced/demo/src/app/signals/playgrounds/playgrounds.component.ts
+++ angular-advanced/demo/src/app/signals/playgrounds/playgrounds.component.ts
@@ -13,20 +13,12 @@ import { PlaygroundsWithSignalsComponent } from "./playgrounds-with-signals.comp
     <div class="container">
       <div class="row gx-5">
         <div class="col">
-          <div class="card">
-            <div class="card-body">
               <h4>With rxjs</h4>
               <loop-playgrounds-with-rxjs/>
             </div>
-          </div>
-        </div>
         <div class="col">
-          <div class="card">
-            <div class="card-body">
               <h4 [style.margin-bottom.px]="62">With signals</h4>
               <loop-playgrounds-with-signals/>
-            </div>
-          </div>
         </div>
       </div>
     </div>
