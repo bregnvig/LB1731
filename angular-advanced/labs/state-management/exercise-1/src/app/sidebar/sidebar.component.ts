c2e3299e8d5f4e9497757a47098d430f6ccf3e8b
Flemming Bregnvig <flemming@nestech.dk>
2025-05-12 10:19:02 +0200

    Add filter

--- angular-advanced/labs/state-management/exercise-1/src/app/sidebar/sidebar.component.ts
+++ angular-advanced/labs/state-management/exercise-1/src/app/sidebar/sidebar.component.ts
@@ -1,14 +1,15 @@
-import { ChangeDetectionStrategy, Component, computed, inject, input, output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, inject, input, output, Signal } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { FaIconComponent } from '@fortawesome/angular-fontawesome';
 import { Playground } from '../model';
 import { DefaultDescriptionPipe, DistancePipe } from "../pipe";
 import { LocationService } from '../service';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'loop-sidebar',
   templateUrl: './sidebar.component.html',
-  imports: [FaIconComponent, DefaultDescriptionPipe, DistancePipe],
+  imports: [FaIconComponent, DefaultDescriptionPipe, DistancePipe, ReactiveFormsModule],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SidebarComponent {
@@ -21,6 +22,15 @@ export class SidebarComponent {
   location = toSignal(inject(LocationService).location$);
 
   loading = computed(() => !this.playgrounds()?.length || !this.location());
+  filterControl = new FormControl<string>('');
+  filteredPlaygrounds: Signal<Playground[] | undefined>;
+
+  constructor() {
+    const filter = toSignal(this.filterControl.valueChanges);
+    this.filteredPlaygrounds = computed(() => {
+      return this.playgrounds()?.filter(p => p.name.toLocaleLowerCase().includes(filter()?.toLocaleLowerCase() ?? ''));
+    });
+  }
 
   selectPlayground(playground: Playground): void {
     this.selectedPlayground = playground;
