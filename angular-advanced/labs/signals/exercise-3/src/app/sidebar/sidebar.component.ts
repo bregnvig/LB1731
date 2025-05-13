c8c523d90b8a811290aad521b357bf9fada317fe
Flemming Bregnvig <flemming@bregnvig.dk>
2025-05-13 08:54:05 +0200

    v19.2

--- angular-advanced/labs/signals/exercise-3/src/app/sidebar/sidebar.component.ts
+++ angular-advanced/labs/signals/exercise-3/src/app/sidebar/sidebar.component.ts
@@ -1,15 +1,13 @@
 import { ChangeDetectionStrategy, Component, input, output } from '@angular/core';
 import { FaIconComponent } from '@fortawesome/angular-fontawesome';
 import { Coordinate, Playground } from '../model';
-import { DefaultDescriptionPipe, DistancePipe } from '../pipe';
 import { SidebarListItemComponent } from './sidebar-list-item.component';
 
 @Component({
   selector: 'loop-sidebar',
-  standalone: true,
   templateUrl: './sidebar.component.html',
-  imports: [FaIconComponent, DefaultDescriptionPipe, DistancePipe, SidebarListItemComponent],
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [FaIconComponent, SidebarListItemComponent],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SidebarComponent {
 
