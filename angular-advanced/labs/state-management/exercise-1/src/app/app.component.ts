5b20a51b8b5de22cffe5b6fab5122ac4a9cf83cb
Flemming Bregnvig <flemming@bregnvig.dk>
2025-05-14 17:32:08 +0200

    Switched to signals

--- angular-advanced/labs/state-management/exercise-1/src/app/app.component.ts
+++ angular-advanced/labs/state-management/exercise-1/src/app/app.component.ts
@@ -1,8 +1,8 @@
-import { AsyncPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, computed, inject, Signal, signal } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { BehaviorSubject, Observable, Subject, combineLatest } from 'rxjs';
-import { distinctUntilChanged, map, startWith, switchMap, tap } from 'rxjs/operators';
+import { BehaviorSubject, combineLatest } from 'rxjs';
+import { distinctUntilChanged, map, switchMap, tap } from 'rxjs/operators';
 import { EditPlaygroundModalComponent } from './edit-playground/edit-playground-modal.component';
 import { ErrorComponent } from "./error.component";
 import { FooterComponent } from "./footer/footer.component";
@@ -10,58 +10,51 @@ import { Center, LeafletModule, Marker } from './leaflet';
 import { Coordinate, Playground } from './model';
 import { LocationService, PlaygroundService } from './service';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { isTruthy } from './utils';
 import { withLength } from './utils/rxjs-utils';
 
 @Component({
   selector: 'loop-root',
   templateUrl: './app.component.html',
-  imports: [FooterComponent, SidebarComponent, AsyncPipe, LeafletModule, ErrorComponent]
+  imports: [FooterComponent, SidebarComponent, LeafletModule, ErrorComponent]
 })
 export class AppComponent {
 
-  playgrounds$: Observable<Playground[]> | undefined;
-  playground$ = new Subject<Playground>();
-  location$: Observable<Coordinate> = this.locationService.location$;
-  center$: Observable<Center>;
-  markers$: Observable<Marker[]> | undefined;
+  #service = inject(PlaygroundService);
+  #modal = inject(NgbModal);
+  #locationService = inject(LocationService);
   #reload = new BehaviorSubject<void>(undefined);
+
+  playgrounds: Signal<Playground[]>;
+  playground = signal<Playground | undefined>(undefined);
+  center: Signal<Center>;
+  markers: Signal<Marker[]> = computed(() => [this.#locationService.location(), this.playground()?.position].filter(isTruthy));
   error: any;
   loading = true;
 
   constructor(
-    private service: PlaygroundService,
-    private modal: NgbModal,
-    private locationService: LocationService,
   ) {
-    this.markers$ = combineLatest([
-      locationService.location$,
-      this.playground$.pipe(map(p => ({ ...p.position, message: p.name })), startWith(undefined)),
-    ]);
-
-    const getDistance = locationService.getDistance;
+    const getDistance = this.#locationService.getDistance;
     const compareLocations = (a: Coordinate, b: Coordinate) => a?.lat === b?.lat && a?.lng === b?.lng;
-    this.playgrounds$ = combineLatest([
-      this.#reload.pipe(switchMap(()  => this.service.list().pipe(withLength()))),
-      locationService.location$.pipe(distinctUntilChanged(compareLocations)),
+    this.playgrounds = toSignal(combineLatest([
+      this.#reload.pipe(switchMap(() => this.#service.list().pipe(withLength()))),
+      this.#locationService.location$.pipe(distinctUntilChanged(compareLocations)),
     ]).pipe(
       map(([playgrounds, location]) =>
         playgrounds
           .sort((a: Playground, b: Playground) => getDistance(a.position, location) - getDistance(b.position, location))
       ),
       tap(() => this.loading = false),
-    );
-    this.center$ = locationService.location$.pipe(
-      startWith({ lat: 56.360029, lng: 10.746635 }),
-      map(location => ({ ...location, zoom: 12 }))
-    );
+    ), { initialValue: [] });
+    this.center = computed(() => this.#locationService.location() ?? { lat: 56.360029, lng: 10.746635 });
   }
 
   async edit(playground: Playground) {
-    EditPlaygroundModalComponent.open(this.modal, playground)
+    EditPlaygroundModalComponent.open(this.#modal, playground)
       .then(() => {
         this.#reload.next();
         this.loading = true;
-    })
+      })
       .catch(error => this.error = error);
   }
 
