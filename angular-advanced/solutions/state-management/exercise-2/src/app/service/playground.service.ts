0045990d43d64c1c44a38af2fc6de1b49f1dafc7
Flemming Bregnvig <flemming@bregnvig.dk>
2025-05-16 13:25:30 +0200

    Copy solution 1 to solution 2

---
+++ angular-advanced/solutions/state-management/exercise-2/src/app/service/playground.service.ts
@@ -0,0 +1,57 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import * as localForage from 'localforage';
+import { defer, from, Observable, of, throwError } from 'rxjs';
+import { delay, map, switchMap, tap } from 'rxjs/operators';
+import { Playground } from '../model';
+import { getNetworkErrorsState } from '../network-errors.component';
+
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PlaygroundService {
+
+  #http = inject(HttpClient);
+  #list = defer(() => localForage.getItem<Playground[]>('playgrounds')).pipe(
+    switchMap(playgrounds => playgrounds ? of(playgrounds) : this.#http.get<Playground[]>('assets/aarhus.json')),
+    switchMap(playgrounds => from(localForage.setItem('playgrounds', playgrounds)).pipe(
+      map(() => playgrounds),
+    )));
+
+  list(): Observable<Playground[]> {
+    return this.#list.pipe(
+      delay(1000), // Simulate network delay
+      tap(() => {
+        if (getNetworkErrorsState().read) throw new Error('Network read error')
+      }),
+    );
+  }
+
+  update(id: string, playground: Partial<Playground>): Observable<Playground> {
+    return this.#list.pipe(
+      map(playgrounds => playgrounds.map(p => p.id === id ? {...p, ...playground} : p)),
+      tap(() => {
+        if (getNetworkErrorsState().update) throw new Error('Network update error')
+      }),
+      switchMap(playgrounds => from(localForage.setItem('playgrounds', playgrounds)).pipe(
+        map(() => playgrounds),
+      )),
+      map(playgrounds => playgrounds.find(p => p.id === id)!),
+    );
+  }
+
+  delete(id: string): Observable<void> {
+    return this.#list.pipe(
+      map(playgrounds => playgrounds.filter(p => p.id !== id)),
+      tap(() => {
+        if (getNetworkErrorsState().delete) throw new Error('Network delete error')
+      }),
+      switchMap(playgrounds => from(localForage.setItem('playgrounds', playgrounds)).pipe(
+        map(() => playgrounds),
+      )),
+      map(() => undefined),
+    );
+  }
+
+}
---
+++ angular-advanced/solutions/state-management/exercise-2/src/app/sidebar/sidebar.component.ts
@@ -0,0 +1,39 @@
+import { ChangeDetectionStrategy, Component, computed, inject, input, model, output, Signal } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import { Playground } from '../model';
+import { DefaultDescriptionPipe, DistancePipe } from "../pipe";
+import { LocationService } from '../service';
+
+@Component({
+  selector: 'loop-sidebar',
+  templateUrl: './sidebar.component.html',
+  imports: [FaIconComponent, DefaultDescriptionPipe, DistancePipe, ReactiveFormsModule],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class SidebarComponent {
+
+  playgrounds = input<Playground[] | null>([]);
+  loading = input<boolean>(false);
+  edit = output<Playground>();
+  delete = output<Playground>();
+
+  selected = model<Playground | undefined>();
+  location = toSignal(inject(LocationService).location$);
+
+  filterControl = new FormControl<string>('');
+  filteredPlaygrounds: Signal<Playground[] | undefined>;
+
+  constructor() {
+    const filter = toSignal(this.filterControl.valueChanges);
+    this.filteredPlaygrounds = computed(() => {
+      return this.playgrounds()?.filter(p => p.name.toLocaleLowerCase().includes(filter()?.toLocaleLowerCase() ?? ''));
+    });
+  }
+
+  selectPlayground(playground: Playground): void {
+    this.selected.set(this.selected() !== playground ? playground : undefined);
+  }
+
+}
