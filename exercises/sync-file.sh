873728c9d36fc68efc40e131abdc03ce38830d09
Flemming Bregnvig <flemming@bregnvig.dk>
2025-09-08 08:25:19 +0200

    Update favicon

---
+++ exercises/sync-file.sh
@@ -0,0 +1,5 @@
+cp ./03-create-first-component/$1 ./04-create-footer-component/$1
+cp ./03-create-first-component/$1 ./05-include-feature-module/$1
+cp ./03-create-first-component/$1 ./06-create-playground-service/$1
+cp ./03-create-first-component/$1 ./07-work-with-async/$1
+cp ./03-create-first-component/$1 ./08-add-some-routes/$1
