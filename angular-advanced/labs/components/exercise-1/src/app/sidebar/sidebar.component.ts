5ea73fd0dff64b81a8d5097cc6ed495a2a363b0c
Flemming Bregnvig <flemming@bregnvig.dk>
2025-09-30 09:35:35 +0200

    Modernize

--- angular-advanced/labs/components/exercise-1/src/app/sidebar/sidebar.component.ts
+++ angular-advanced/labs/components/exercise-1/src/app/sidebar/sidebar.component.ts
@@ -1,17 +1,16 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, input, output } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Coordinate, Playground } from '../model';
 import { LocationService } from '../service';
 import { DefaultDescriptionPipe } from '../pipe/default-description.pipe';
 import { HumanizeDistancePipe } from '../pipe/humanize-distance.pipe';
 import { DistancePipe } from '../pipe/distance.pipe';
-import { NgFor, AsyncPipe } from '@angular/common';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
     selector: 'loop-sidebar',
     templateUrl: './sidebar.component.html',
     imports: [
-        NgFor,
         AsyncPipe,
         DistancePipe,
         HumanizeDistancePipe,
@@ -20,8 +19,8 @@ import { NgFor, AsyncPipe } from '@angular/common';
 })
 export class SidebarComponent {
 
-  @Input() playgrounds: Playground[] | null = [];
-  @Output() selected = new EventEmitter<Playground>();
+  playgrounds = input<Playground[] | null>([]);
+  selected = output<Playground>();
 
   selectedPlayground?: Playground;
   location$: Observable<Coordinate> = this.locationService.location$;
