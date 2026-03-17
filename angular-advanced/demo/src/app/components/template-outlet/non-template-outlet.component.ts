14c8104fa7d1a24846ea1273c0d9e09b9415d40d
Flemming Bregnvig <flemming@bregnvig.dk>
2026-03-17 09:52:20 +0100

    Typed control

--- angular-advanced/demo/src/app/components/template-outlet/non-template-outlet.component.ts
+++ angular-advanced/demo/src/app/components/template-outlet/non-template-outlet.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { UntypedFormControl } from '@angular/forms';
+import { FormControl } from '@angular/forms';
 import { combineLatest } from 'rxjs';
 import { debounceTime, map, startWith } from 'rxjs/operators';
 import { PlaygroundService } from 'src/app/shared';
@@ -12,7 +12,7 @@ import { PlaygroundService } from 'src/app/shared';
 export class NonTemplateOutletComponent implements OnInit {
 
   playgrounds$ = this.service.playgrounds$;
-  filterControl = new UntypedFormControl();
+  filterControl = new FormControl<string>('', { nonNullable: true });
 
   constructor(private service: PlaygroundService) { }
 
