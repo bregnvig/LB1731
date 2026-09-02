6673b0b8f736db8ee180f6fb9bef129ee1a4fba0
Flemming Bregnvig <flemming@bregnvig.dk>
2026-09-02 08:40:43 +0200

    Move slides

--- zipit.sh
+++ zipit.sh
@@ -1,3 +1,5 @@
 zip -r solutions solutions -x "**/node_modules/*" -x "**/dist/*" -x "**/.angular/*" -x "**/di/*" -x ".git" -x "**/*.sh"
 zip -r demo demo -x "**/node_modules/*" -x "**/dist/*" -x "**/.angular/*" -x "**/di/*" -x ".git" -x "**/*.sh"
 zip -r exercises exercises -x "**/node_modules/*" -x "**/dist/*" -x "**/.angular/*" -x "**/di/*" -x ".git" -x "**/*.sh"
+zip -r slides slides -x "**/node_modules/*" -x "**/dist/*" -x "**/.angular/*" -x "**/di/*" -x ".git" -x "**/*.sh"
+zip -r slides-exercises slides-exercises -x "**/node_modules/*" -x "**/dist/*" -x "**/.angular/*" -x "**/di/*" -x ".git" -x "**/*.sh"
